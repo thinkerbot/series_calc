69e5ab6075a1face2935ba5c2b0ed8814db12221
Simon Chiang <simon.a.chiang@gmail.com>
2014-04-14 08:17:37 -0600

    Merge Node into Term
    Term turns out to be the lowest level; dimensions are looking like they will
    have to manage data on each of the terms, meaning there is no place for a
    data-less node.

--- lib/series_calc/term.rb
+++ lib/series_calc/term.rb
@@ -1,7 +1,5 @@
-require 'series_calc/node'
-
 module SeriesCalc
-  class Term < Node
+  class Term
     class << self
       def subclass(&block)
         subclass = Class.new(Term)
@@ -24,13 +22,114 @@ module SeriesCalc
       end
     end
 
+    attr_reader :identifier
+    attr_reader :parents
+    attr_reader :children
     attr_reader :data
 
     def initialize(identifier = nil, data = nil)
-      super(identifier)
+      @identifier = identifier
+      @parents  = []
+      @children = []
       @dependents = nil
       @data = data
       @value = nil
+    end
+
+    def walk_parents(&block) # :yields: node, path
+      walk(:parents, &block)
+    end
+
+    def walk_children(&block) # :yields: node, path
+      walk(:children, &block)
+    end
+
+    def attach_parents(*parents)
+      parents = parents - self.parents
+      parents.each do |parent|
+        self.attach_parent(parent)
+        parent.attach_child(self)
+      end
+
+      walk_parents do |node, path|
+        if node == self
+          raise "cycle detected: #{path.map(&:identifier).map(&:inspect).join(' -> ')}"
+        end
+      end
+
+      parents.each do |parent|
+        parent.each_at_and_above(&:recalculate_value)
+      end
+      each_at_and_below(&:recalculate_dependents)
+    end
+
+    def detach_parents(*parents)
+      parents = self.parents & parents
+      parents.each do |parent|
+        self.detach_parent(parent)
+        parent.detach_child(self)
+      end
+
+      parents.each do |parent|
+        parent.each_at_and_above(&:recalculate_value)
+      end
+      each_at_and_below(&:recalculate_dependents)
+    end
+
+    def attach_children(*children)
+      children = children - self.children
+      children.each do |child|
+        child.attach_parent(self)
+        self.attach_child(child)
+      end
+
+      walk_children do |node, path|
+        if node == self
+          raise "cycle detected: #{path.map(&:identifier).map(&:inspect).join(' -> ')}"
+        end
+      end
+
+      children.each do |child|
+        child.each_at_and_below(&:recalculate_dependents)
+      end
+      each_at_and_above(&:recalculate_value)
+    end
+
+    def detach_children(*children)
+      children = self.children & children
+      children.each do |child|
+        child.detach_parent(self)
+        self.detach_child(child)
+      end
+
+      children.each do |child|
+        child.each_at_and_below(&:recalculate_dependents)
+      end
+      each_at_and_above(&:recalculate_value)
+    end
+
+    def each_at_and_above(&block)
+      unless block_given?
+        return enum_for(:each_at_and_above)
+      end
+
+      yield self
+      parents.each do |parent|
+        parent.each_at_and_above(&block)
+      end
+      self
+    end
+
+    def each_at_and_below(&block)
+      unless block_given?
+        return enum_for(:each_at_and_below)
+      end
+
+      yield self
+      children.each do |child|
+        child.each_at_and_below(&block)
+      end
+      self
     end
 
     def recalculate_dependents
@@ -41,8 +140,8 @@ module SeriesCalc
       @dependents ||= each_at_and_above.to_a.uniq.freeze
     end
 
-    def data=(new_data)
-      @data = new_data
+    def data=(data)
+      @data = data
       dependents.each(&:recalculate_value)
     end
 
@@ -62,36 +161,31 @@ module SeriesCalc
       raise NotImplementedError
     end
 
-    def attach_parents(*parents)
-      super
-      parents.each do |parent|
-        parent.each_at_and_above(&:recalculate_value)
+    protected
+
+    def walk(method_name, path = [self], &block)
+      send(method_name).each do |node|
+        path.push node
+        block.call(node, path)
+        node.walk(method_name, path, &block)
+        path.pop
       end
-      each_at_and_below(&:recalculate_dependents)
     end
 
-    def detach_parents(*parents)
-      super
-      parents.each do |parent|
-        parent.each_at_and_above(&:recalculate_value)
-      end
-      each_at_and_below(&:recalculate_dependents)
+    def attach_parent(parent)
+      parents << parent
     end
 
-    def attach_children(*children)
-      super
-      children.each do |child|
-        child.each_at_and_below(&:recalculate_dependents)
-      end
-      each_at_and_above(&:recalculate_value)
+    def detach_parent(parent)
+      parents.delete(parent)
     end
 
-    def detach_children(*children)
-      super
-      children.each do |child|
-        child.each_at_and_below(&:recalculate_dependents)
-      end
-      each_at_and_above(&:recalculate_value)
+    def attach_child(child)
+      children << child
+    end
+
+    def detach_child(child)
+      children.delete(child)
     end
   end
 end
--- test/series_calc/term_test.rb
+++ test/series_calc/term_test.rb
@@ -6,6 +6,136 @@ class SeriesCalc::TermTest < Test::Unit::TestCase
   Term = SeriesCalc::Term
 
   #
+  # attach_parents
+  #
+
+  def test_attach_parents_adds_child_to_parent_and_vice_versa
+    parent, child = Term.new, Term.new
+    child.attach_parents(parent)
+
+    assert_equal [parent], child.parents
+    assert_equal [child], parent.children
+  end
+
+  def test_attach_parents_raises_error_if_attached_to_self
+    term = Term.new("term")
+    err = assert_raises(RuntimeError) { term.attach_parents(term) }
+    assert_equal 'cycle detected: "term" -> "term"', err.message
+  end
+
+  def test_attach_parents_raises_error_if_cycle_is_detected
+    a, b, c = %w{a b c}.map {|identifier| Term.new(identifier) }
+
+    c.attach_parents(b)
+    b.attach_parents(a)
+
+    err = assert_raises(RuntimeError) { a.attach_parents(c) }
+    assert_equal 'cycle detected: "a" -> "c" -> "b" -> "a"', err.message
+  end
+
+  #
+  # detach_parents
+  #
+
+  def test_detach_parents_removes_child_from_parent_and_vice_versa
+    parent, child_a, child_b = Term.new, Term.new, Term.new
+    child_a.attach_parents(parent)
+    child_b.attach_parents(parent)
+
+    assert_equal [parent], child_a.parents
+    assert_equal [parent], child_b.parents
+    assert_equal [child_a, child_b], parent.children
+
+    child_b.detach_parents(parent)
+
+    assert_equal [parent], child_a.parents
+    assert_equal [], child_b.parents
+    assert_equal [child_a], parent.children
+  end
+
+  #
+  # attach_children
+  #
+
+  def test_attach_children_adds_parent_to_child_and_vice_versa
+    parent, child = Term.new, Term.new
+    parent.attach_children(child)
+
+    assert_equal [child], parent.children
+    assert_equal [parent], child.parents
+  end
+
+  def test_attach_children_raises_error_if_attached_to_self
+    term = Term.new("term")
+    err = assert_raises(RuntimeError) { term.attach_children(term) }
+    assert_equal 'cycle detected: "term" -> "term"', err.message
+  end
+
+  def test_attach_children_raises_error_if_cycle_is_detected
+    a, b, c = %w{a b c}.map {|identifier| Term.new(identifier) }
+
+    a.attach_children(b)
+    b.attach_children(c)
+
+    err = assert_raises(RuntimeError) { c.attach_children(a) }
+    assert_equal 'cycle detected: "c" -> "a" -> "b" -> "c"', err.message
+  end
+
+  #
+  # detach_children
+  #
+
+  def test_detach_children_removes_parent_from_child_and_vice_versa
+    parent_a, parent_b, child = Term.new, Term.new, Term.new
+    parent_a.attach_children(child)
+    parent_b.attach_children(child)
+
+    assert_equal [child], parent_a.children
+    assert_equal [child], parent_b.children
+    assert_equal [parent_a, parent_b], child.parents
+
+    parent_b.detach_children(child)
+
+    assert_equal [child], parent_a.children
+    assert_equal [], parent_b.children
+    assert_equal [parent_a], child.parents
+  end
+
+  #
+  # each_at_and_above
+  #
+
+  def test_each_at_and_above_yields_self_and_all_ancestors
+    terms = %w{a0 a1 a2 a3 b0 b1 c0}
+    a0, a1, a2, a3, b0, b1, c0 = terms.map {|identifier| Term.new(identifier) }
+
+    c0.attach_parents(b0, b1)
+    b0.attach_parents(a0, a1)
+    b1.attach_parents(a2, a3)
+
+    assert_equal %w{a0 a1 a2 a3 b0 b1 c0}, c0.each_at_and_above.map(&:identifier).sort
+    assert_equal %w{a0 a1 b0}, b0.each_at_and_above.map(&:identifier).sort
+    assert_equal %w{a0}, a0.each_at_and_above.map(&:identifier).sort
+  end
+
+  #
+  # each_at_and_below
+  #
+
+  def test_each_at_and_below_yields_self_and_all_descendants
+    terms = %w{a0 b0 b1 c0 c1 c2 c3}
+    a0, b0, b1, c0, c1, c2, c3 = terms.map {|identifier| Term.new(identifier) }
+
+    a0.attach_children(b0, b1)
+    b0.attach_children(c0, c1)
+    b1.attach_children(c2, c3)
+
+    assert_equal %w{a0 b0 b1 c0 c1 c2 c3}, a0.each_at_and_below.map(&:identifier).sort
+    assert_equal %w{b0 c0 c1}, b0.each_at_and_below.map(&:identifier).sort
+    assert_equal %w{c0}, c0.each_at_and_below.map(&:identifier).sort
+  end
+
+  #
   # dependents
   #
 
